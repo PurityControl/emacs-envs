02c0484f18a7db317696b29be012a113b0466b63
Craig Ferry <PurityControl@users.noreply.github.com>
2015-01-14 07:17:20 -0800

    create repo

---
+++ clojure/init.el
@@ -0,0 +1,34 @@
+(setq user-emacs-directory "~/emacs-configs/clojure/")
+
+(setq make-backup-files nil)
+
+;; changes all yes/no questions to y/n type
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; PACKAGE INSTALLATION
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; add melpa stable to package repos
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+
+;; list the packages you want to install - will be preserved in src control
+(defvar my-packages '(magit
+                      evil
+		      clojure-mode
+		      cider))
+
+;; install the packages  if not installed
+(dolist (p my-packages)
+  (unless (package-installed-p p)
+    (package-install p)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
